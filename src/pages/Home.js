f6bcdf6a8485160f6050e232b17b63ea5e76338d
frllk <1402805300@qq.com>
2020-07-20 02:31:48 +0800

    setState 实现同步的三种方式

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
 
 export default class Home extends Component {
+
+  // constructor(props) {
+  //   super(props)
+  //   this.setCounter = this.setCounter.bind(this) // 解决：直接定义函数方法找不到this问题 ~~~
+  // }
+
   // 使用state属性维护状态
   state = {
-    date: new Date()
+    date: new Date(),
+    counter: 0 // setState 同步异步问题**************************
+
   }
 
   // 挂载时
@@ -15,6 +23,17 @@ export default class Home extends Component {
         date: new Date()
       })
     }, 1000);
+
+    /* // 方式三
+    document.getElementsByTagName('button')[0].addEventListener('click', () => {
+      this.setState({
+        counter: this.state.counter + 1
+      })
+      this.setState({
+        counter: this.state.counter + 2
+      })
+      console.log('sta', this.state.counter);
+    }) */
   }
 
   // 卸载时
@@ -23,15 +42,59 @@ export default class Home extends Component {
     clearInterval(this.timerId)
   }
 
+  // setCounter () {
+  //   // 问题：这么写会找不到this ===> 推荐使用箭头函数 ~~~
+  //   this.setState({
+  //     counter: this.state.counter + 1
+  //   })
+  // }
+
+  setCounter = () => {
+    // setState 异步
+    /* this.setState({
+      counter: this.state.counter + 1
+    })
+    this.setState({
+      counter: this.state.counter + 2
+    }, () => {
+      console.log('sta', this.state.counter);
+    }) */
+    // setState 实现同步==========================
+    /* // 方式一
+    this.setState((nextState) => {
+      return {
+        counter: nextState.counter + 1
+      }
+    })
+    this.setState((nextState) => {
+      return {
+        counter: nextState.counter + 2
+      }
+    }) */
+    // 方式二
+    setTimeout(() => {
+      this.setState({
+        counter: this.state.counter + 1
+      })
+      this.setState({
+        counter: this.state.counter + 2
+      })
+      console.log('sta-cb', this.state.counter); // 新值
+    }, 0);
+    console.log('sta', this.state.counter); // 旧值
+  }
+
   render () {
     const str = '我是home页面'
-    const { date } = this.state
+    const { date, counter } = this.state
     return (
       <div>
         <h1>
           {str}
         </h1>
         <p>{date.toLocaleString()}</p>
+        <p>{counter}</p>
+        <button onClick={this.setCounter}>修改counter</button>
       </div>
     )
   }
