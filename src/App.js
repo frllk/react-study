2f0b1e886ee464c6b15d5f1f0abeb303df2fe807
frllk <1402805300@qq.com>
2020-07-20 03:58:37 +0800

    Props属性传递

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import Home from './pages/Home.js'
 import User from './pages/User.js'
 import Search from './pages/Search.js'
 
+const store = {
+  userInfo: {
+    userName: 'xiaoming',
+    age: 18
+  }
+}
+
 function App () {
   return (
     <div className="App">
       {/* <Home /> */}
       {/* <User /> */}
-      <Search />
+      <Search store={store} />
     </div>
   );
 }
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 
 export default class Search extends Component {
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     name: ''
+  //   }
+  // }
   state = {
     name: ''
   }
@@ -16,10 +22,13 @@ export default class Search extends Component {
     console.log('handlerChange', this.state.name);
   }
   render () {
+    console.log('search页面', this)
     const { name } = this.state
+    const { userInfo } = this.props.store
     return (
       <div >
         <h1>我是Search页面</h1>
+        <p>{userInfo.userName}-{userInfo.age}</p>
         <button onClick={this.handlerInp}>click</button>
         <input value={name} onChange={this.handlerChange} type="text" />
       </div >
