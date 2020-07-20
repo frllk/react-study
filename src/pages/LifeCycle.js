c28e20dec4ae2b8c3e6c1beafb32b9e54fd367a5
frllk <1402805300@qq.com>
2020-07-20 12:18:28 +0800

    生命周期

--- src/pages/LifeCycle.js
+++ src/pages/LifeCycle.js
@@ -11,19 +11,35 @@ export default class LifeCycle extends Component {
     }
     console.log('constructor', this.state.counter);
   }
-  // 组件挂载之前
+  /* // 组件挂载之前
   componentWillMount () {
     console.log('componentWillMount', this.state.counter);
-  }
+  } */
   // 组件挂载之后
   componentDidMount () {
     console.log('componentDidMount', this.state.counter);
   }
 
-  // 组件更新之前
+  /* // 组件更新之前
   componentWillUpdate () {
     console.log('componentWillUpdate', this.state.counter);
+  } */
+
+  static getDerivedStateFromProps (props, state) {
+    // getDerivedStateFromProps 会在调用 render 方法之前调用。并且在初始挂载及后续更新时都会被调用。
+    // 它应该返回一个对象来更新 state，如果返回 null  则不更新任何内容
+    const { counter } = state
+    console.log('getDerivedStateFromProps', counter);
+    return counter < 8 ? null : { counter: 0 }
   }
+
+  // 在render之后，在componentDidUpdate之前。
+  getSnapshotBeforeUpdate (prevProps, prevState) {
+    const { counter } = prevState
+    console.log('getSnapshotBeforeUpdate', counter);
+    return null
+  }
+
   // 组件更新之后
   componentDidUpdate () {
     console.log('componentDidUpdate', this.state.counter);
@@ -56,8 +72,11 @@ export default class LifeCycle extends Component {
         <h1>我是LifeCycle页面</h1>
         <p>{counter}</p>
         <button onClick={this.setCounter}>改变counter</button>
-        {
+        {/* {
           !!(counter % 2) && <Foo />
+        } */}
+        {
+          <Foo counter={counter} />
         }
       </div>
     )
@@ -71,8 +90,9 @@ class Foo extends Component {
   }
   render () {
     return (
-      <div>
+      <div style={{ border: '2px solid #999', margin: '10px' }}>
         <h1>我是Foo组件</h1>
+        <p>Foo counter: {this.props.counter}</p>
       </div>
     )
   }
